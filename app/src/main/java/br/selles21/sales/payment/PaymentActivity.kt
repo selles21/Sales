cafa4d7caa5ad7153daa656ff667b9ed3006f9f1
Andrés <andresselles21@gmail.com>
2020-01-20 11:40:36 -0300

    Last changes
    Updating app logo.
    Deleting comments.
    Changing app name

--- app/src/main/java/br/selles21/sales/payment/PaymentActivity.kt
+++ app/src/main/java/br/selles21/sales/payment/PaymentActivity.kt
@@ -27,7 +27,6 @@ class PaymentActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        setContentView(R.layout.activity_payment)
         activityPaymentBinding = DataBindingUtil.setContentView(
             this,
             R.layout.activity_payment
